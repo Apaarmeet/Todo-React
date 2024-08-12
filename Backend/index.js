f96f36715c2b9907a8565c06d091aead5de96e28
apaarmeet <apaarmeet5000@gmail.com>
2024-08-12 18:21:19 +0530

    updating routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
 app.post('/todo', (req, res) => {
-  
+  const createPayload = req.body;
+  const parsedpayload = createTodo.safeParse(createPayload);
+
+  if(!parsedpayload.success){
+    res.status(411).json({
+      msg:"You sent the wrong inputs"
+    })
+    return;
+  }
+
 })
 
 app.get('/todo', (req, res) => {
@@ -13,7 +23,15 @@ app.get('/todo', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
-    
+    const updatePayload = req.body;
+    const parsedpayload = updateTodo.safeParse(updatePayload);
+
+    if(!parsedpayload){
+      res.status(411).json({
+        msg:"You sent the wrong inputs"
+      })
+      return;
+    }
 })
 
 
