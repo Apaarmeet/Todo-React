6cf100e87c23db5c405e041b52cdbd24cdb0b78d
apaarmeet <apaarmeet5000@gmail.com>
2024-08-13 11:39:37 +0530

    ummm

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://admin:WAsQ6qxYbgoI4huN@cluster0.zfe9bjg.mongodb.net/todos")
 
-const todoSchema  = mongoose.schema({
+const todoSchema  = mongoose.Schema({
     title:String,
     description:String,
     completed:Boolean
